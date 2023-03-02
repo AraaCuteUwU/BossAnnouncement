48445219fc8859db33a835561476ee513e41707e
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2023-03-02 20:02:31 +0800

    PM4

--- src/xenialdan/BossAnnouncement/EventListener.php
+++ src/xenialdan/BossAnnouncement/EventListener.php
@@ -2,34 +2,40 @@
 
 namespace xenialdan\BossAnnouncement;
 
-use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
-class EventListener implements Listener
-{
+class EventListener implements Listener {
 
-    public function onJoin(PlayerJoinEvent $ev): void
-    {
-        if (Loader::getInstance()->isWorldEnabled($ev->getPlayer()->getLevel()->getName())) {
+    /**
+     * @param PlayerJoinEvent $ev
+     * @return void
+     */
+    public function onJoin(PlayerJoinEvent $ev): void {
+        if (Loader::getInstance()->isWorldEnabled($ev->getPlayer()->getWorld()->getFolderName())) {
             Loader::getInstance()->bar->addPlayer($ev->getPlayer());
         }
     }
 
-    public function onLeave(PlayerQuitEvent $ev): void
-    {
+    /**
+     * @param PlayerQuitEvent $ev
+     * @return void
+     */
+    public function onLeave(PlayerQuitEvent $ev): void {
         Loader::getInstance()->bar->removePlayer($ev->getPlayer());
     }
 
-    public function onLevelChange(EntityLevelChangeEvent $ev): void
-    {
-        if ($ev->isCancelled() || !$ev->getEntity() instanceof Player) {
-            return;
-        }
+    /**
+     * @param EntityTeleportEvent $ev
+     * @return void
+     */
+    public function onLevelChange(EntityTeleportEvent $ev): void {
+        if ($ev->isCancelled() || !$ev->getEntity() instanceof Player) return;
         Loader::getInstance()->bar->removePlayer($ev->getEntity());
-        if (Loader::getInstance()->isWorldEnabled($ev->getTarget()->getName())) {
+        if (Loader::getInstance()->isWorldEnabled($ev->getTo()->getWorld()->getFolderName())) {
             Loader::getInstance()->bar->addPlayer($ev->getEntity());
         }
     }
