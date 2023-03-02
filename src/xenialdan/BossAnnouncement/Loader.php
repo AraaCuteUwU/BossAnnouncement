109ed5f32039d2c5be9a5e9459a4b706a2663609
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2023-03-02 19:50:19 +0800

    Fix

--- src/xenialdan/BossAnnouncement/Loader.php
+++ src/xenialdan/BossAnnouncement/Loader.php
@@ -7,42 +7,32 @@
  * A simple boss bar tile plugin using apibossbar
  */
 
-namespace xenialdan\BossAnnouncement;
-
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\PluginException;
 use pocketmine\scheduler\Task;
+use pocketmine\utils\SingletonTrait;
 use pocketmine\utils\TextFormat;
 use xenialdan\apibossbar\DiverseBossBar;
 
-class Loader extends PluginBase implements Listener
-{
-    /** @var DiverseBossBar */
-    public $bar;
-    public $title = '', $subTitles = [], $changeSpeed = 0, $i = 0;
-    public static $instance;
+class Loader extends PluginBase implements Listener {
+    use SingletonTrait;
 
-    public function onLoad(): void
-    {
-        self::$instance = $this;
-    }
+    /** @var DiverseBossBar $bar */
+    public DiverseBossBar $bar;
+    /** @var int $i */
+    public int $i = 0;
+    /** @var int $changeSpeed */
+    public int $changeSpeed = 0;
+    /** @var array $subTitles */
+    public array $subTitles = [];
+    /** @var string $title */
+    public string $title = '';
 
-    /**
-     * @return self
-     */
-    public static function getInstance(): self
-    {
-        return self::$instance;
-    }
 
-    /**
-     * @throws PluginException
-     */
-    public function onEnable(): void
-    {
+    public function onEnable(): void {
+        self::setInstance($this);
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
         $this->title = $this->getConfig()->get('head-message', '');
@@ -50,14 +40,13 @@ class Loader extends PluginBase implements Listener
         $this->changeSpeed = max(1, $this->getConfig()->get('change-speed', 1));
         $this->bar = (new DiverseBossBar())->setTitle($this->title);//setTitle needed?
         $this->getScheduler()->scheduleRepeatingTask(new class extends Task {
-            public function onRun(int $currentTick): void
-            {
+            public function onRun(): void {
                 Loader::getInstance()->i++;
                 if (Loader::getInstance()->i >= count(Loader::getInstance()->subTitles)) {
                     Loader::getInstance()->i = 0;
                 }
                 foreach (Loader::getInstance()->bar->getPlayers() as $player) {
-                    if ($player->isOnline() && Loader::getInstance()->isWorldEnabled($player->getLevel()->getName())) {
+                    if ($player->isOnline() && Loader::getInstance()->isWorldEnabled($player->getWorld()->getFolderName())) {
                         Loader::getInstance()->setText($player);
                     }
 
@@ -95,14 +84,8 @@ class Loader extends PluginBase implements Listener
      * @param string $text
      * @return string
      */
-    public function formatText(Player $player, string $text): string
-    {
-        // preg_match_all ("/(\{.*?\})/ig", $text, $brackets);
-
-        //TODO auto function
-        $text = str_replace(['{display_name}', '{name}', '{x}', '{y}', '{z}', '{world}', '{level_players}', '{server_players}', '{server_max_players}', '{hour}', '{minute}', '{second}', '{BLACK}', '{DARK_BLUE}', '{DARK_GREEN}', '{DARK_AQUA}', '{DARK_RED}', '{DARK_PURPLE}', '{GOLD}', '{GRAY}', '{DARK_GRAY}', '{BLUE}', '{GREEN}', '{AQUA}', '{RED}', '{LIGHT_PURPLE}', '{YELLOW}', '{WHITE}', '{OBFUSCATED}', '{BOLD}', '{STRIKETHROUGH}', '{UNDERLINE}', '{ITALIC}', '{RESET}', '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r'], [$player->getDisplayName(), $player->getName(), $player->getFloorX(), $player->getFloorY(), $player->getFloorZ(), ($level = $player->getLevel()) !== null ? $level->getName() : '', count($player->getLevel()->getPlayers()), count($player->getServer()->getOnlinePlayers()), $player->getServer()->getMaxPlayers(), date('H'), date('i'), date('s'), '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r', TextFormat::BLACK, TextFormat::DARK_BLUE, TextFormat::DARK_GREEN, TextFormat::DARK_AQUA, TextFormat::DARK_RED, TextFormat::DARK_PURPLE, TextFormat::GOLD, TextFormat::GRAY, TextFormat::DARK_GRAY, TextFormat::BLUE, TextFormat::GREEN, TextFormat::AQUA, TextFormat::RED, TextFormat::LIGHT_PURPLE, TextFormat::YELLOW, TextFormat::WHITE, TextFormat::OBFUSCATED, TextFormat::BOLD, TextFormat::STRIKETHROUGH, TextFormat::UNDERLINE, TextFormat::ITALIC, TextFormat::RESET], $text);
-
-        return $text;
+    public function formatText(Player $player, string $text): string {
+        return str_replace(['{display_name}', '{name}', '{x}', '{y}', '{z}', '{world}', '{level_players}', '{server_players}', '{server_max_players}', '{hour}', '{minute}', '{second}', '{BLACK}', '{DARK_BLUE}', '{DARK_GREEN}', '{DARK_AQUA}', '{DARK_RED}', '{DARK_PURPLE}', '{GOLD}', '{GRAY}', '{DARK_GRAY}', '{BLUE}', '{GREEN}', '{AQUA}', '{RED}', '{LIGHT_PURPLE}', '{YELLOW}', '{WHITE}', '{OBFUSCATED}', '{BOLD}', '{STRIKETHROUGH}', '{UNDERLINE}', '{ITALIC}', '{RESET}', '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r'], [$player->getDisplayName(), $player->getName(), $player->getPosition()->getX(), $player->getPosition()->getY(), $player->getPosition()->getZ(), ($world = $player->getWorld()) !== null ? $world->getFolderName() : '', count($player->getWorld()->getPlayers()), count($player->getServer()->getOnlinePlayers()), $player->getServer()->getMaxPlayers(), date('H'), date('i'), date('s'), '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r', TextFormat::BLACK, TextFormat::DARK_BLUE, TextFormat::DARK_GREEN, TextFormat::DARK_AQUA, TextFormat::DARK_RED, TextFormat::DARK_PURPLE, TextFormat::GOLD, TextFormat::GRAY, TextFormat::DARK_GRAY, TextFormat::BLUE, TextFormat::GREEN, TextFormat::AQUA, TextFormat::RED, TextFormat::LIGHT_PURPLE, TextFormat::YELLOW, TextFormat::WHITE, TextFormat::OBFUSCATED, TextFormat::BOLD, TextFormat::STRIKETHROUGH, TextFormat::UNDERLINE, TextFormat::ITALIC, TextFormat::RESET], $text);
     }
 
     /**
@@ -116,22 +99,19 @@ class Loader extends PluginBase implements Listener
             return strtolower(TextFormat::clean($worldName));
         }, $this->getConfig()->get('worlds', []));
         $levelName = strtolower(TextFormat::clean($levelName));
-        switch ($mode) {
-            case 0://Every world
-                return true;
-                break;
-            case 1://Only config worlds
-                return in_array($levelName, $configWorlds, true);
-                break;
-            case 2://Exclude config worlds
-                return !in_array($levelName, $configWorlds, true);
-                break;
-        }
-        return false;
+        return match ($mode) {
+            0 => true,
+            1 => in_array($levelName, $configWorlds, true),
+            2 => !in_array($levelName, $configWorlds, true),
+            default => false,
+        };
     }
 
-    public function onDeath(PlayerDeathEvent $ev)
-    {
+    /**
+     * @param PlayerDeathEvent $ev
+     * @return void
+     */
+    public function onDeath(PlayerDeathEvent $ev): void {
         $this->bar->removePlayer($ev->getPlayer())->addPlayer($ev->getPlayer());
     }
 }
